36bfc3b701be5c31fb936a2cef4456e46405408d
alexLeonov44 <72029072+alexLeonov44@users.noreply.github.com>
2022-10-01 10:02:42 +0300

    Update StartPage.jsx

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link, NavLink, useHistory} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 export default function StartPage() {
     const history = useHistory();
